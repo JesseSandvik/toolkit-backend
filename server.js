2cda5cb976ee20025ac164a862a4ef7a51bfc2fd
Jesse Sandvik <sandvikjesse@gmail.com>
2022-02-17 23:46:57 -0500

    api calls setup for local json file, endpoints functioning as expected

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const employeesRouter = require('./routes/api/employees');
+const rootRouter = require('./routes/root');
+const subdirRouter = require('./routes/subdir');
 const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
-const res = require('express/lib/response');
 const PORT = process.env.PORT || 5000;
 
 // custom middleware logger
@@ -36,20 +38,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serves static files
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //sends 302 by default
-});
+app.use('/', rootRouter);
+app.use('/subdir', subdirRouter);
+app.use('/employees', employeesRouter);
 
 const one = (req, res, next) => {
     console.log('one');
@@ -68,8 +62,15 @@ const three = (req, res, next) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
 });
 
 app.use(errorHandler);
